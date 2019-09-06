ad6fb2b3394b029df9f02f91d5b0204664b76209
Aman_arcanion <amannnk27@gmail.com>
2019-09-07 01:56:04 +0530

    basic console functions

---
+++ JS_LEVEL_ONE/part_one.js
@@ -0,0 +1,4 @@
+alert("Hello World");
+var dep = prompt("How much would you like to deposit today?");
+alert("You have deposited "+ dep);
+console.log("you are a cool person");
---
+++ JS_LEVEL_ONE/practise.js
@@ -0,0 +1,4 @@
+var pouw = prompt("Enter your weight in pounds(lbs)")
+var kgw = pouw * 0.454
+alert("You weigh "+kgw+" kgs!")
+console.log("Conversion Completed")
